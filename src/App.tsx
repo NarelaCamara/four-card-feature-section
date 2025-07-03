ed7fdd3f309d59654a255daee63490dd75824dd4
RoxanaCamara <roxana.camara07@gmail.com>
2025-07-03 02:26:07 -0300

    feat: desktop desing

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,35 @@ import { useState } from "react";
 import "./App.css";
 import { Card } from "./components/card";
 import home from "./assets/icon-homepage.svg";
-import { colors, type ICard } from "./utils";
+import { colors } from "./utils";
 
 function App() {
-  const [card] = useState<ICard>({
-    title: "Supervisor",
-    color: colors[0],
-    text: "Monitors activity to identify project roadblocks",
-    icon: home,
-  });
+  const [cards] = useState([
+    {
+      title: "Supervisor",
+      color: colors[0],
+      text: "Monitors activity to identify project roadblocks",
+      icon: home,
+    },
+    {
+      title: "Supervisor",
+      color: colors[1],
+      text: "Monitors activity to identify project roadblocks",
+      icon: home,
+    },
+    {
+      title: "Supervisor",
+      color: colors[2],
+      text: "Monitors activity to identify project roadblocks",
+      icon: home,
+    },
+    {
+      title: "Supervisor",
+      color: colors[3],
+      text: "Monitors activity to identify project roadblocks",
+      icon: home,
+    },
+  ]);
 
   return (
     <div className="flex flex-col flex-nowrap items-center m-8 min-w-[330px] md:min-w-[768px]">
@@ -24,18 +44,33 @@ function App() {
         Our Artificial Intelligence powered tools use millions of project data
         points to ensure that your project is successful
       </p>
-      <div className="grid md:grid-cols-3 md:grid-rows-3 gap-0">
+      <div className="grid lg:grid-cols-3 lg:grid-rows-3 lg:gap-10 ">
+        <div className=" lg:col-start-1  lg:row-start-2">
+          <Card {...cards[0]} /> {/**verde */}
+        </div>
+        <div className=" lg:col-start-2 lg:row-start-1 lg:relative lg:top-[12rem]">
+          <Card {...cards[1]} /> {/**rojo */}
+        </div>
+        <div className="lg:col-start-2 lg:row-start-3 lg:relative lg:bottom-[12rem]">
+          <Card {...cards[2]} /> {/**amarillo */}
+        </div>
+        <div className="lg:col-start-3 lg:row-start-2  ">
+          <Card {...cards[3]} /> {/**azul */}
+        </div>
+      </div>
+
+      <div className=" grid md:grid-cols-3 md:grid-rows-3 md:gap-0 ">
         <div className=" md:col-start-2">
-          <Card {...card} />
+          <Card {...cards[0]} />
         </div>
         <div className=" md:col-start-1 md:row-start-2 md:relative md:left-[7rem]">
-          <Card {...card} />
+          <Card {...cards[1]} />
         </div>
         <div className="md:col-start-3 md:row-start-2 md:relative md:right-[7rem]">
-          <Card {...card} />
+          <Card {...cards[2]} />
         </div>
         <div className="md:col-start-2 md:row-start-3">
-          <Card {...card} />
+          <Card {...cards[3]} />
         </div>
       </div>
     </div>
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,4 +1,9 @@
-export const colors = ["border-t-[#EA5454]"] as const;
+export const colors = [
+  "border-t-[#44D3D2]",
+  "border-t-[#EA5454]",
+  "border-t-[#FCAE4A]",
+  "border-t-[#549EF2]",
+] as const;
 export type Color = (typeof colors)[number];
 
 export interface ICard {
